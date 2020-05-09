3820fe68f6fabe4b2c8bd55786c75545db639d00
= <rickard_hallerback@hotmail.com>
2020-05-09 11:34:18 +0200

    command lines

--- main.c
+++ main.c
@@ -1,10 +1,53 @@
 #include "y.tab.h"
 #include "ast.h"
 #include "hooks.h"
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
+void usage(char *argv0, int ret)
+{
+	fprintf((ret == 0 ? stdout : stderr),
+		"usage: %s [-p] [-i] [-h|--help]\r\n",
+		argv0);
+	fprintf((ret == 0 ? stdout : stderr),
+		"  -p: print AST\r\n");
+	fprintf((ret == 0 ? stdout : stderr),
+		"  -i: launch intepreter\r\n");
+	fprintf((ret == 0 ? stdout : stderr),
+		"  -h|--help: print this help\r\n");
+	fprintf((ret == 0 ? stdout : stderr), "\r\n");
+	fprintf((ret == 0 ? stdout : stderr), 
+		"%s compiled: %s %s\r\n", argv0, __DATE__, __TIME__);
+	exit(ret);
+}
+
+typedef enum mission {
+	runAsIntepreter,
+	runAsASTPrinter
+} mission_t;
+
+int main(int argc, char *argv[]) {
+	mission_t mission = runAsASTPrinter;
 	statement_t *root = NULL;
 	MainParserFunc parse;
+	int ret = 0;
+
+	/* Check inputs */
+	if ( argc > 1 ) {
+		int i = 1;
+		while ( i < argc ) {
+			if ( strcmp("-p", argv[i]) == 0){
+				mission = runAsASTPrinter;
+			} else if ( strcmp("-i", argv[i]) == 0){
+				mission = runAsIntepreter;
+			} else if ( strcmp("-h", argv[i]) == 0){
+				usage(argv[0], 0);
+			} else if ( strcmp("--help", argv[i]) == 0){
+				usage(argv[0], 0);
+			}
+			++i;
+		}
+	} 
 
 	/* Initialize hooks */
 	initParser();
@@ -13,13 +56,29 @@ int main() {
 	parse = getParser();
 
 	/* Parse the program */
-    parse();
+	parse();
 
-    /* Get the root statement */
-    root = getRoot();
+	/* Get the root statement */
+	root = getRoot();
 
-    if ( root != NULL ) {
-    	/* Printt the program */
-        print_statements(root);
-    }
+	switch ( mission ) {
+	break;
+	case runAsIntepreter:
+		fprintf(stderr, "INTERPRETER NOT IMPLEMENTED YET\r\n");
+		ret = 1;
+		break;
+	case runAsASTPrinter:
+		if ( root != NULL ) {
+			/* Printt the program */
+			print_statements(root);
+		} else {
+			fprintf(stderr, "Failed to parse program!\r\n");
+			ret = 1;
+		}
+	break;
+	default:
+	break;
+	}
+
+	return ret;
 }

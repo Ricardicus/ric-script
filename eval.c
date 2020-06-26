8913f1438d79e53d2e310ddb73e8929315b4d7e2
= <rickard_hallerback@hotmail.com>
2020-06-27 00:51:40 +0200

    Arguments and parameters fix

--- eval.c
+++ eval.c
@@ -3,13 +3,13 @@
 /* Namespace (all global) */
 hashtable_t *funcDecs = NULL;
 hashtable_t *varDecs = NULL;
-hashtable_t *arguments = NULL;
 
 jmp_buf jmpbuf;
 
 int evaluate_condition(ifCondition_t *cond,
   PROVIDE_CONTEXT_ARGS(),
-  argsList_t* args)
+  argsList_t* args,
+  hashtable_t *argVals)
 {
   /* Will set ax either 1 or 0 (or interrupt the program on error) */
   stackval_t svLeft;
@@ -18,10 +18,10 @@ int evaluate_condition(ifCondition_t *cond,
   /* Arbitrary double resoultion, not sure what to set this to */
   double epsilon = 0.00001;
 
-  evaluate_expression(cond->left, PROVIDE_CONTEXT(), args);
+  evaluate_expression(cond->left, PROVIDE_CONTEXT(), args, argVals);
   POP_VAL(&svLeft, sp);
 
-  evaluate_expression(cond->right, PROVIDE_CONTEXT(), args);
+  evaluate_expression(cond->right, PROVIDE_CONTEXT(), args, argVals);
   POP_VAL(&svRight, sp);
 
   switch (cond->type) 
@@ -272,7 +272,8 @@ int evaluate_condition(ifCondition_t *cond,
 void evaluate_expression(
   expr_t *expr,
   PROVIDE_CONTEXT_ARGS(),
-  argsList_t* args)
+  argsList_t* args,
+  hashtable_t *argVals)
 {
   if ( expr == NULL )
     return;
@@ -282,33 +283,62 @@ void evaluate_expression(
     case EXPR_TYPE_ID:
     {
       heapval_t *hv;
+      int stop = 0;
 
       /* Check if this ID is among the arguments */
       argsList_t *walk = args;
 
-      while ( walk != NULL ) {
+      while ( walk != NULL && !stop ) {
         expr_t *exp = walk->arg;
         if ( exp->type == EXPR_TYPE_ID ) {
           expr_t *expArg;
 
           /* Check among the variables if we have it defined there */
-          expArg = hashtable_get(arguments, expr->id.id);
+          expArg = hashtable_get(argVals, expr->id.id);
 
           if ( expArg != NULL ) {
             /* This was an argument! */
-            if ( expArg->type == EXPR_TYPE_ID ) {
-              // This is not supposed to happen!
-            } else {
-              /* Evaluate this expression */
-              evaluate_expression(expArg, PROVIDE_CONTEXT(), args);
+            switch ( expArg->type ) {
+            case EXPR_TYPE_IVAL:
+              PUSH_INT(expArg->ival, sp);
+              break;
+            case EXPR_TYPE_FVAL:
+              PUSH_DOUBLE(expArg->fval, sp);
+              break;
+            case EXPR_TYPE_TEXT:
+            {
+              size_t len = strlen(expArg->text);
+              stackval_t sv;
+              heapval_t *hvp;
+              char *newText = ast_emalloc(len+1);
+              snprintf(newText, len+1, "%s", expArg->text);
+
+              sv.type = TEXT;
+              sv.t = newText;
+
+              ALLOC_HEAP(&sv, hp, &hvp);
+
+              PUSH_STRING(sv.t, sp);
+              break;
             }
-            break;
+            default:
+              fprintf(stderr, "error: Invalid usage of identifier '%s'\n", expr->id.id);
+              exit(1);
+              break;
+            }
+
+            stop = 1;
           }
         } else {
-          printf("walk->arg not exp type ID (%d)\n", exp->type);
+          fprintf(stderr, "error: unknown, this is crazy. The interpreter is borken or something.\n");
+          exit(1);
         }
 
         walk = walk->next;
+      }
+
+      if ( stop ) {
+        break;
       }
 
       if ( walk == NULL ) {
@@ -385,10 +415,10 @@ void evaluate_expression(
       stackval_t svLeft;
       stackval_t svRight;
 
-      evaluate_expression((expr_t*)expr->add.left, PROVIDE_CONTEXT(), args);
+      evaluate_expression((expr_t*)expr->add.left, PROVIDE_CONTEXT(), args, argVals);
       POP_VAL(&svLeft, sp);
 
-      evaluate_expression((expr_t*)expr->add.right, PROVIDE_CONTEXT(), args);
+      evaluate_expression((expr_t*)expr->add.right, PROVIDE_CONTEXT(), args, argVals);
       POP_VAL(&svRight, sp);
 
       switch (svLeft.type) {
@@ -520,10 +550,10 @@ void evaluate_expression(
       stackval_t svLeft;
       stackval_t svRight;
 
-      evaluate_expression((expr_t*)expr->add.left, PROVIDE_CONTEXT(), args);
+      evaluate_expression((expr_t*)expr->add.left, PROVIDE_CONTEXT(), args, argVals);
       POP_VAL(&svLeft, sp);
 
-      evaluate_expression((expr_t*)expr->add.right, PROVIDE_CONTEXT(), args);
+      evaluate_expression((expr_t*)expr->add.right, PROVIDE_CONTEXT(), args, argVals);
       POP_VAL(&svRight, sp);
 
       switch (svLeft.type) {
@@ -583,10 +613,10 @@ void evaluate_expression(
       stackval_t svLeft;
       stackval_t svRight;
 
-      evaluate_expression((expr_t*)expr->add.left, PROVIDE_CONTEXT(), args);
+      evaluate_expression((expr_t*)expr->add.left, PROVIDE_CONTEXT(), args, argVals);
       POP_VAL(&svLeft, sp);
 
-      evaluate_expression((expr_t*)expr->add.right, PROVIDE_CONTEXT(), args);
+      evaluate_expression((expr_t*)expr->add.right, PROVIDE_CONTEXT(), args, argVals);
       POP_VAL(&svRight, sp);
 
       switch (svLeft.type) {
@@ -646,10 +676,10 @@ void evaluate_expression(
       stackval_t svLeft;
       stackval_t svRight;
 
-      evaluate_expression((expr_t*)expr->add.left, PROVIDE_CONTEXT(), args);
+      evaluate_expression((expr_t*)expr->add.left, PROVIDE_CONTEXT(), args, argVals);
       POP_VAL(&svLeft, sp);
 
-      evaluate_expression((expr_t*)expr->add.right, PROVIDE_CONTEXT(), args);
+      evaluate_expression((expr_t*)expr->add.right, PROVIDE_CONTEXT(), args, argVals);
       POP_VAL(&svRight, sp);
 
       switch (svLeft.type) {
@@ -706,10 +736,10 @@ void evaluate_expression(
       stackval_t svLeft;
       stackval_t svRight;
 
-      evaluate_expression((expr_t*)expr->add.left, PROVIDE_CONTEXT(), args);
+      evaluate_expression((expr_t*)expr->add.left, PROVIDE_CONTEXT(), args, argVals);
       POP_VAL(&svLeft, sp);
 
-      evaluate_expression((expr_t*)expr->add.right, PROVIDE_CONTEXT(), args);
+      evaluate_expression((expr_t*)expr->add.right, PROVIDE_CONTEXT(), args, argVals);
       POP_VAL(&svRight, sp);
 
       switch (svLeft.type) {
@@ -767,7 +797,7 @@ void evaluate_expression(
     break;
     case EXPR_TYPE_COND:
     {
-      evaluate_condition(expr->cond, PROVIDE_CONTEXT(), args);
+      evaluate_condition(expr->cond, PROVIDE_CONTEXT(), args, argVals);
       /* Push ax to stack */
       PUSH_INT(*ax,sp);
       break;
@@ -781,7 +811,8 @@ void evaluate_expression(
 void interpret_statements_(
   void *stmt,
   PROVIDE_CONTEXT_ARGS(),
-  argsList_t *args
+  argsList_t *args,
+  hashtable_t *argVals
 )
 {
   entity_eval_t *eval = (entity_eval_t*)stmt;
@@ -803,7 +834,7 @@ void interpret_statements_(
     { 
       stackval_t sv;
       expr_t *e = ((statement_t*)stmt)->content;
-      evaluate_expression(e, PROVIDE_CONTEXT(), args);
+      evaluate_expression(e, PROVIDE_CONTEXT(), args, argVals);
       POP_VAL(&sv, sp);
       switch ( sv.type) {
       case INT32TYPE:
@@ -845,7 +876,7 @@ void interpret_statements_(
       declaration_t* decl = ((statement_t*)stmt)->content;
 
       /* Evaluating the expression among global variables */
-      evaluate_expression(decl->val, PROVIDE_CONTEXT(), args);
+      evaluate_expression(decl->val, PROVIDE_CONTEXT(), args, argVals);
       POP_VAL(&sv, sp);
 
       /* Placing value on the heap */
@@ -869,7 +900,7 @@ void interpret_statements_(
 
       stackval_t sv;
       expr_t *e = sys_stmt->content;
-      evaluate_expression(e, PROVIDE_CONTEXT(), args);
+      evaluate_expression(e, PROVIDE_CONTEXT(), args, argVals);
       POP_VAL(&sv, sp);
       switch ( sv.type ) {
       case TEXT:
@@ -900,7 +931,7 @@ void interpret_statements_(
     case LANG_ENTITY_BREAK:
     {
       /* Set PC to break 'end' */
-      interpret_statements_(*ed, PROVIDE_CONTEXT(), args);
+      interpret_statements_(*ed, PROVIDE_CONTEXT(), args, argVals);
     }
     break;
     case LANG_ENTITY_FUNCCALL:
@@ -909,6 +940,7 @@ void interpret_statements_(
       functionCall_t *funcCall = ((statement_t*)stmt)->content;
       argsList_t *argsWalk = funcCall->args;
       argsList_t *params;
+      hashtable_t *newArgumentTable = new_argstable();
 
       /* Looking up the function and calling it if it exists */
       funcDef = hashtable_get(funcDecs, funcCall->id.id);
@@ -943,9 +975,6 @@ void interpret_statements_(
           exit(1);
         }
 
-        /* flush arguments */
-        flush_arguments();
-
         /* Populate arguments */
         while ( argsWalk != NULL && params != NULL ) {
           stackval_t sv;
@@ -958,7 +987,7 @@ void interpret_statements_(
           }
 
           /* Evaluate expression */
-          evaluate_expression(argsWalk->arg, PROVIDE_CONTEXT(), args);
+          evaluate_expression(argsWalk->arg, PROVIDE_CONTEXT(), args, argVals);
           
           /* Fetch the evaluated expression to the arguments table */
           POP_VAL(&sv, sp);
@@ -983,7 +1012,7 @@ void interpret_statements_(
           }
 
           /* Adding expression to argument table */
-          hashtable_put(arguments, params->arg->id.id, newArg);
+          hashtable_put(newArgumentTable, params->arg->id.id, newArg);
 
           params = params->next;
           argsWalk = argsWalk->next;
@@ -992,7 +1021,10 @@ void interpret_statements_(
       }
 
       /* Call the function */
-      interpret_statements_(funcDef->body, PROVIDE_CONTEXT(), funcDef->params);
+      interpret_statements_(funcDef->body, PROVIDE_CONTEXT(), funcDef->params, newArgumentTable);
+      
+      /* Free the argument value table */
+      flush_arguments(newArgumentTable);
     }
     break;
     case LANG_ENTITY_CONDITIONAL:
@@ -1012,20 +1044,20 @@ void interpret_statements_(
       (*(uintptr_t*)ed) = (uintptr_t) next;
 
       /* Read ax for conditional */
-      evaluate_condition(ifstmt->cond, PROVIDE_CONTEXT(), args);
+      evaluate_condition(ifstmt->cond, PROVIDE_CONTEXT(), args, argVals);
       if ( *ax ) {
         interpret_statements_(ifstmt->body,
-          PROVIDE_CONTEXT(), args);
+          PROVIDE_CONTEXT(), args, argVals);
       } else {
         // Walk through the elifs.
         int stop = 0;
         ifstmtWalk = ifstmt->elif;
 
         while ( ifstmtWalk != NULL ) {
-          evaluate_condition(ifstmtWalk->cond, PROVIDE_CONTEXT(), args);
+          evaluate_condition(ifstmtWalk->cond, PROVIDE_CONTEXT(), args, argVals);
           if ( *ax ) {
             interpret_statements_(ifstmtWalk->body,
-              PROVIDE_CONTEXT(), args);
+              PROVIDE_CONTEXT(), args, argVals);
             stop = 1;
             break;
           }
@@ -1036,7 +1068,7 @@ void interpret_statements_(
           // Print the else if it is not NULL
           if ( ifstmt->endif != NULL ) {
             ifstmtWalk = ifstmt->endif;
-            interpret_statements_(ifstmtWalk->body, PROVIDE_CONTEXT(), args);
+            interpret_statements_(ifstmtWalk->body, PROVIDE_CONTEXT(), args, argVals);
           }
         }
 
@@ -1053,7 +1085,7 @@ void interpret_statements_(
     break;
   }
 
-  interpret_statements_(next, PROVIDE_CONTEXT(), args);
+  interpret_statements_(next, PROVIDE_CONTEXT(), args, argVals);
 }
 
 void setup_namespaces() {
@@ -1061,14 +1093,19 @@ void setup_namespaces() {
   assert(funcDecs != NULL);
   varDecs = hashtable_new(200, 0.8);
   assert(varDecs != NULL);
-  arguments = hashtable_new(20, 0.8);
-  assert(arguments != NULL);
-  arguments->data_also = 1;
 }
 
 void close_namespaces() {
   hashtable_free(funcDecs);
   hashtable_free(varDecs);
+}
+
+hashtable_t *new_argstable()
+{
+  hashtable_t *argTable = hashtable_new(20, 0.8);
+  assert(argTable != NULL);
+  argTable->data_also = 1;
+  return argTable;
 }
 
 void print_expr(expr_t *expr)
@@ -1186,13 +1223,10 @@ void print_indents(int indent) {
   }
 }
 
-void flush_arguments()
+void flush_arguments(hashtable_t *table)
 {
-  if ( arguments != NULL ) {
-    hashtable_free(arguments);
-    arguments = hashtable_new(20, 0.8);
-    assert(arguments != NULL);
-    arguments->data_also = 1;
+  if ( table != NULL ) {
+    hashtable_free(table);
   }
 }
 
@@ -1362,7 +1396,7 @@ void interpret_statements(statement_t *stmt)
 
   if ( setjmp(jmpbuf) == 0) {
     /* Start descending and evaluating the AST */
-    interpret_statements_(stmt, PROVIDE_CONTEXT_INIT(), NULL);
+    interpret_statements_(stmt, PROVIDE_CONTEXT_INIT(), NULL, NULL);
   } else {
     // Close namespaces
     close_namespaces();

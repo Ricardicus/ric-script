90b26b221be409d5653886a60ad2c8371d6a83cb
Rickard <rickard.hallerback@gmail.com>
2020-08-11 16:33:52 +0200

    Adding tests for function pointer

---
+++ tests/test_func_ptr.py
@@ -0,0 +1,21 @@
+import os
+
+def test_file_exist():
+
+  file = "test.txt"
+  output_lines = [
+"Foobar!",
+"Foobar! foo",
+"Foobar! foobar",
+"Foobar! foo bar :)"
+  ]
+
+  lib_script = "./../builddir/ric ../samples/func_ptr.ric "
+
+  ric_result = os.popen(lib_script).read().splitlines()
+
+  assert len(output_lines) == len(ric_result)
+
+  for i in range(0,len(ric_result)):
+    assert output_lines[i] == ric_result[i]
+

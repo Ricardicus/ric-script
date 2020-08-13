2e15f5c1f08fc636be30749c5d2722211866e084
Rickard <rickard.hallerback@gmail.com>
2020-08-13 17:56:18 +0200

    adding math tests

---
+++ tests/test_math.py
@@ -0,0 +1,39 @@
+import os
+from settings import *
+
+def test_vector_print():
+  output_lines = [
+"2.000000",
+"2.828427",
+"1.414214",
+"8.000000",
+"16.000000",
+"32.000000",
+"4.605170",
+"5.298317",
+"6.907755",
+"0.001593",
+"0.000000",
+"-0.999999",
+"1.000000",
+"-0.001593",
+"0.000000",
+"2.718282",
+"7.389056",
+"2.000000",
+"2.000000",
+"1337.000000",
+"1337.000000",
+"1337.000000",
+"1338.000000"
+  ]
+
+  lib_script = "./" + EXECUTABLE + " ../samples/math.ric "
+
+  ric_result = os.popen(lib_script).read().splitlines()
+
+  assert len(output_lines) == len(ric_result)
+
+  for i in range(0,len(ric_result)):
+    assert output_lines[i] == ric_result[i]
+

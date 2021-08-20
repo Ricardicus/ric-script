616b25ba69e24040e6cd316f771a65a9f05e69cf
Rickard <rickard.hallerback@gmail.com>
2021-08-20 12:59:28 +0200

    Windows test fix

--- tests/test_data_print.py
+++ tests/test_data_print.py
@@ -8,9 +8,11 @@ def test_file_exist():
 "ABCDEFGHIJKLMNOPQRSTUVWXY",
 "YXWVUTSRQPONMLKJIHGFEDCBA",
 "GIKMOQS",
-"SQOMKIG",
-"[119,104,101,101,108,10,112,121,116,101,115,116,10,10,10]"
-  ]
+"SQOMKIG"]
+
+  if ( os.name != 'nt' ):
+    output_lines.append(
+      "[119,104,101,101,108,10,112,121,116,101,115,116,10,10,10]")
 
   lib_script = callSample('data_print.ric')
 
